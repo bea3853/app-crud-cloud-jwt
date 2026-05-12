a58ba3acb33ab39568933a7be751bca27560bb5b
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-05-12 17:35:08 -0300

    Update app_view.py

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -2,10 +2,7 @@ import streamlit as st
 import requests
 import os
 
-# API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
-
-# API_URL = "[https://app-crud-gtob.onrender.com](https://app-crud-gtob.onrender.com)"
-API_URL = os.getenv("API_URL", "[https://app-crud-gtob.onrender.com](https://app-crud-gtob.onrender.com)")
+API_URL = "https://app-crud-gtob.onrender.com"
 
 st.set_page_config(page_title="Biblioteca Digital", layout="wide")
 
